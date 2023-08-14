61d484a7b9b028cca864b3d69751f02ef4361f02
delaram majestic <majesticdelaram@yahoo.com>
2023-08-14 10:09:20 +0330

    feat: add hri string generator

--- pkg/hri/human_readable_id.go
+++ pkg/hri/human_readable_id.go
@@ -1,1 +1,25 @@
 package hri
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+var random *rand.Rand
+var letters = []byte("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func init() {
+	source := rand.NewSource(time.Now().UnixNano())
+	random = rand.New(source)
+}
+
+func Create(prefix string, length int) string {
+
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = letters[random.Intn(len(letters))]
+	}
+
+	return fmt.Sprintf(prefix+"%s-%s", string(b), time.Now().Format("060102"))
+}
