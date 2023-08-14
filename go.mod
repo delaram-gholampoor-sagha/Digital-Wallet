07381730b6daa7ee830098a6d4a1e7d0af6810d8
delaram majestic <majesticdelaram@yahoo.com>
2023-08-14 10:11:20 +0330

    feat: add log functionality

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/delaram-gholampoor-sagha/Digital-Wallet
 
 go 1.19
+
+require go.uber.org/zap v1.25.0
+
+require go.uber.org/multierr v1.10.0 // indirect
--- pkg/log/log.go
+++ pkg/log/log.go
@@ -1,1 +1,32 @@
 package log
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+type Config struct {
+	OutputPaths       []string
+	ErrorOutputPaths  []string
+	DisableStacktrace bool
+	Level             zapcore.Level
+}
+
+func New(service string, cfg Config) (*zap.SugaredLogger, error) {
+	zapConfig := zap.NewProductionConfig()
+	zapConfig.OutputPaths = cfg.OutputPaths
+	zapConfig.ErrorOutputPaths = cfg.ErrorOutputPaths
+	zapConfig.EncoderConfig.EncodeTime = zapcore.RFC3339TimeEncoder
+	zapConfig.DisableStacktrace = cfg.DisableStacktrace
+	zapConfig.Level.SetLevel(cfg.Level)
+	zapConfig.InitialFields = map[string]interface{}{
+		"service": service,
+	}
+
+	logger, err := zapConfig.Build()
+	if err != nil {
+		return nil, err
+	}
+
+	return logger.Sugar(), nil
+}
